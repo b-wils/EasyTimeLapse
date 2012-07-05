9f5cf2de3f0d1f4163061e61f1b994a2b5c4ab03
Brandon Wilson <bmw8719@gmail.com>
2012-07-05 15:58:04 +0000

    Importing ETL-comm-test
    git-svn-id: https://subversion.assembla.com/svn/etl_webapp/trunk@85 3fa7648a-587c-4a36-b6a6-96aee5749d1d

---
+++ iOS/ETL-comm-test/ETL-comm-test/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  ETL-comm-test
+//
+//  Created by Inspired Eye on 6/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
