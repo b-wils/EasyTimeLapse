f21aa5646ac109e6da9c63c4ca2d34cbc27cd411
revcbh <revcbh>
2012-07-14 19:12:11 +0000

    Adding external testing dependencies
    git-svn-id: https://subversion.assembla.com/svn/etl_webapp/trunk@118 3fa7648a-587c-4a36-b6a6-96aee5749d1d

---
+++ iOS/ETL-interface-test/ETLTests/ETLDeviceInterfaceTest.mm
@@ -0,0 +1,13 @@
+//
+//  ETLDeviceInterfaceTest.m
+//  ETL-interface-test
+//
+//  Created by Carll Hoffman on 7/12/12.
+//  Copyright (c) 2012 Microsoft. All rights reserved.
+//
+
+#import "ETLDeviceInterfaceTest.h"
+
+@implementation ETLDeviceInterfaceTest
+
+@end
